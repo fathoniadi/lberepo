4f2162bf9a39453579183e06a084e571509b3432
nawakula <nasa.nakula@gmail.com>
2016-11-30 06:47:21 +0700

    update

--- resources/views/dashboard/layout/dashboard.blade.php
+++ resources/views/dashboard/layout/dashboard.blade.php
@@ -1,28 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>LBE - AJK</title>
-
-<link href="{{url('/assets')}}/css/bootstrap.min.css" rel="stylesheet">
-<link href="{{url('/assets')}}/css/datepicker3.css" rel="stylesheet">
-<link href="{{url('/assets')}}/css/styles.css" rel="stylesheet">
-<link href="{{url('/assets')}}/css/bootstrap-multiselect.css" rel="stylesheet">
-<link href="{{url('/assets')}}/css/bootstrap-table.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
-<!-- <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-<script>tinymce.init({ selector:'textarea' });</script> -->
-<script src="{{url('/assets')}}/js/lumino.glyphs.js"></script>
-
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>LBE - AJK</title>
+	<link href="{{url('/assets')}}/css/bootstrap.min.css" rel="stylesheet">
+	<link href="{{url('/assets')}}/css/datepicker3.css" rel="stylesheet">
+	<link href="{{url('/assets')}}/css/styles.css" rel="stylesheet">
+	<link href="{{url('/assets')}}/css/bootstrap-multiselect.css" rel="stylesheet">
+	<link href="{{url('/assets')}}/css/bootstrap-table.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+	<script src="{{url('/assets')}}/js/lumino.glyphs.js"></script>
 </head>
 
 <body>
 	
 	@yield('navbar')
 	@yield('sidebar')
-	
-
 	@yield('content')
 
 	<script src="{{url('/assets')}}/js/jquery-1.11.1.min.js"></script>
@@ -34,6 +28,31 @@
 	<script src="{{url('/assets')}}/js/easypiechart-data.js"></script>
 	<script src="{{url('/assets')}}/js/bootstrap-datepicker.js"></script>
 	<script src="{{url('/assets')}}/js/bootstrap-table.js"></script>
+	<script src="{{url('/bower_components')}}/tinymce/tinymce.min.js"></script>
+	<script>
+		tinymce.init({
+			selector: '#eventNote',
+			height: 500,
+			theme: 'modern',
+		  	plugins: [
+			    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+			    'searchreplace wordcount visualblocks visualchars code fullscreen',
+			    'insertdatetime media nonbreaking save table contextmenu directionality',
+			    'emoticons template paste textcolor colorpicker textpattern imagetools codesample'
+		  	],
+		  	toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+		  	toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+		  	image_advtab: true,
+		 	templates: [
+				{ title: 'Test template 1', content: 'Test 1' },
+			  	{ title: 'Test template 2', content: 'Test 2' }
+		  	],
+		  	content_css: [
+			    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+			    '//www.tinymce.com/css/codepen.min.css'
+		  	]
+		 });
+	</script>
 	<script type="text/javascript">
 	    $(document).ready(function() {
 	       $('.selectpicker').multiselect({
@@ -43,7 +62,6 @@
 	    });
 	</script>
 	<script>
-		
 		$('#calendar').datepicker({
 		});
 
@@ -61,7 +79,5 @@
 		  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
 		})
 	</script>
-
 </body>
-
 </html>
